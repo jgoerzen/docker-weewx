b2b868588a3f5ccaf8b8f19f098438fe190ebaee
RXM307 <seven@seven.dorksville.net>
2020-12-29 21:42:27 +1000

    Update to python3 and 4.2.0

--- setup/setup.sh
+++ setup/setup.sh
@@ -4,8 +4,8 @@ set -e
 set -x
 
 cd /tmp/setup
-wget "http://www.weewx.com/downloads/released_versions/weewx_${WEEWX_VERSION}-1_all.deb"
+wget "http://www.weewx.com/downloads/released_versions/python3-weewx_${WEEWX_VERSION}-1_all.deb"
 sha256sum -c < sums
-dpkg -i "weewx_${WEEWX_VERSION}-1_all.deb" || apt-get -y --no-install-recommends -f install
-rm "weewx_${WEEWX_VERSION}-1_all.deb"
+dpkg -i "python3-weewx_${WEEWX_VERSION}-1_all.deb" || apt-get -y --no-install-recommends -f install
+rm "python3-weewx_${WEEWX_VERSION}-1_all.deb"
 
