8c9d9bccf6c3d10840fd27da4998117ff100f430
John Goerzen <jgoerzen@complete.org>
2024-04-04 06:32:29 -0500

    Attempt to work around issue

--- setup/setup.sh
+++ setup/setup.sh
@@ -12,6 +12,10 @@ adduser --uid 995 --gecos 'weewx' --system --group weewx
 
 wget "http://www.weewx.com/downloads/released_versions/python3-weewx_${WEEWX_VERSION}-1_all.deb"
 sha256sum -c < sums
-dpkg -i "python3-weewx_${WEEWX_VERSION}-1_all.deb" || apt-get -y --no-install-recommends -f install
+
+# Expect dpkg to fail due to lack of prereqs
+#
+# Have to supply USER here due to a bug
+USER=root dpkg -i "python3-weewx_${WEEWX_VERSION}-1_all.deb" || apt-get -y --no-install-recommends -f install
 rm "python3-weewx_${WEEWX_VERSION}-1_all.deb"
 
